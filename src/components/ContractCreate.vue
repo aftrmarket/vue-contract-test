7f46bff27fa5707cd84c2b4ce2dfe8d55b60e076
arcj0 <baumerj@gmail.com>
2023-04-12 09:08:28 -0400

    chore: tested new aftr-js version

--- src/components/ContractCreate.vue
+++ src/components/ContractCreate.vue
@@ -274,8 +274,8 @@ export default {
         },
         async buttonPress2() {
             let repo = {
-                name: "dApp - TST648",
-                ticker: "dTST648"
+                name: "dApp - TST752",
+                ticker: "dTS752"
             };
             let response = await createRepo(repo, "use_wallet", undefined, this.network);
             if (response.status === "success") {
